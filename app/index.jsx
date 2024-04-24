f13c002de4209ab84f4b4e4d5320a540c6e97b6f
Kevin Sebastian <116342133+kevinssheva@users.noreply.github.com>
2024-04-24 22:05:48 +0700

    feat: bottom-tabs

---
+++ app/index.jsx
@@ -0,0 +1,20 @@
+import { Link } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-3xl font-pblack">Aora!</Text>
+      <Link
+        href="/home"
+        style={{
+          color: "blue",
+        }}
+      >
+        Home
+      </Link>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
